92407dbdfeb4640256f68277234ae6a6b443d6ce
Studio_upGrad <studio_upgrad@UE-M-NOTE-0012.local>
2020-02-01 15:59:56 +0530

    Exercise -> Composition

--- src/com/ims/actor/Admin.java
+++ src/com/ims/actor/Admin.java
@@ -4,21 +4,15 @@ class Admin {
     //DataType nameofattribute;
     int id;
     String  name;
-    String buildingNumber;
-    String streetName;
-    String city;
-    int pincode;
+    Address address;
 
     Admin() {
         System.out.println("Admin constructed !!");
     }
 
-    Admin(String n, String bn, String sn, String c, int pc) {
+    Admin(String n, Address address) {
         name=n;
-        buildingNumber=bn;
-        streetName=sn;
-        city=c;
-        pincode=pc;
+        this.address = address;
     }
 
     // returnType methodName(){ //method definition -> return statement}
