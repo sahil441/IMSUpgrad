bc19932fd9fedd8cf036104c356e6f72d1d394d4
Studio_upGrad <studio_upgrad@UE-M-NOTE-0012.local>
2020-02-01 14:28:53 +0530

    Exercise -> Inventory Manager

---
+++ src/com/ims/actor/InventoryManager.java
@@ -0,0 +1,16 @@
+package com.ims.actor;
+
+class InventoryManager {
+    int id;
+    String  name;
+    String buildingNumber;
+    String streetName;
+    String city;
+    int pincode;
+
+    // returnType methodName(){ //method definition -> return statement}
+    void checkProfileDashboard() {
+        System.out.println("Please selection an option : ");
+        System.out.println("Press 1 for Viewing Profile Details");
+    }
+}
