3c14fcafa07c6927870c49d5a90df37aa803f08e
Studio_upGrad <studio_upgrad@UE-M-NOTE-0012.local>
2020-02-01 15:46:46 +0530

    Exercise -> Supplier class with constructors

--- src/com/ims/actor/Supplier.java
+++ src/com/ims/actor/Supplier.java
@@ -1,4 +1,25 @@
 package com.ims.actor;
 
 class Supplier {
+
+    int id;
+    String  name;
+    String buildingNumber;
+    String streetName;
+    String city;
+    int pincode;
+
+    Supplier() {
+        System.out.println("Supplier Constructed!");
+    }
+
+    Supplier(String name, String buildingNumber, String streetName, String city, int pincode) {
+        this();
+        this.name = name;
+        this.buildingNumber = buildingNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.pincode = pincode;
+        System.out.println("Assignments Done");
+    }
 }
